069b392c4eec31d09926ebefab0259da7ff07675
unknown <dori1233>
2025-04-12 19:46:07 +0700

    update public, css

---
+++ public/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Welcome</title>
+
+<!-- AOS CSS -->
+<link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+
+<!-- STYLE CSS -->
+<link rel="stylesheet" href="../assets/css/index/index.css">
+
+
+</head>
+
+<body>
+
+
+<!-- Hero Section -->
+<section class="hero-section">
+<div class="container">
+    <div class="row align-items-center">
+        <!-- Teks -->
+        <div class="col-md-6 mb-4 mb-md-0" data-aos="fade-right" data-aos-duration="1000">
+            <h1 class="display-4 fw-bold text-brown">Selamat Datang di Web <span class="text-warning">MISOPU FINANCE</span></h1>
+            <p class="lead text-secondary mb-4">Hitung Pendapatan Anda 🍜<br>Buat dari manual menjadi lebih mudah</p>
+            <a href="roles/users/dashboard.php" class="btn btn-misopu me-3">Masuk</a>
+            <!-- <a href="roles/users/menu.php" class="btn btn-outline-secondary">Lihat Menu</a> -->
+        </div>
+        <!-- Gambar -->
+        <div class="col-md-6 text-center" data-aos="fade-left" data-aos-duration="1000">
+            <img src="../assets/img/misopu_logo.png" class="img-fluid rounded shadow" alt="Ilustrasi makanan">
+        </div>
+        </div>
+
+</div>
+</section>
+
+
+
+
+
+<!-- AOS JS -->
+<script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+AOS.init();
+</script>
+
+
+
+
+</body>
+</html>
